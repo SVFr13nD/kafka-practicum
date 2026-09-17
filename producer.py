db572b160ccedfe0898a64ff0b7cde5bcaf04102
Зимин Александр Александрович <Alexandrhe.Zimin@kaspi.kz>
2026-09-17 13:54:48 +0500

    Initial commit

---
+++ producer.py
@@ -0,0 +1,10 @@
+from confluent_kafka import Producer
+conf = {"bootstrap.servers":"kps-grf1-lp1:9094"}
+producer = Producer(conf)
+producer.produce(
+    topic="yandex_practicum_2",
+    key="key-1",
+    value="message-1"
+)
+producer.flush()
+print("Message sent!")
